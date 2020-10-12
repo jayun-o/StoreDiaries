bc6ae8d7b140138d1bbb7a9d3c50a63cb533a182
wannabe2266 <wannasiri.somb@dome.tu.ac.th>
2020-10-13 00:09:52 +0700

    register and login

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,78 @@
+@extends('layouts.app')
+
+@section('content') 
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Login</div>
+
+                <div class="panel-body">
+                @if (session('status'))
+                    <div class="bg-red-500 p-4 rounded-lg mb-6 text-white text-center">
+                        {{ session('status') }}
+                    </div>
+
+
+                @endif
+                    <form class="form-horizontal" role="form" method="post" action="{{ route('login') }}">
+                        @csrf
+
+                        <!-- Email -->
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">E-Mail Address</label>
+
+                            <div class="col-md-6">
+                                <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+                                
+                                @error('email')
+                                <div class="text-red-500 mt-2 text-sm">
+                                    {{ $message }}
+                                </div>
+                                @enderror
+
+                            </div>
+                        </div>
+
+                        <!-- Password -->
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">Password</label>
+
+                            <div class="col-md-6">
+                                <input type="password" class="form-control" name="password">
+                                
+                                @error('password')
+                                <div class="text-red-500 mt-2 text-sm">
+                                    {{ $message }}
+                                </div>
+                                @enderror
+
+                            </div>
+                        </div>
+
+                        <!-- Check box -->
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox" name="remember"> Remember Me
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Login button -->
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    <i class="fa fa-btn fa-sign-in"></i>Login
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
