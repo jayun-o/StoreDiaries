a0398a633ddb9a325549afe2e848bedb411afa4c
Nattanicha Ramphan <nattanicha.ram@dome.tu.ac.th>
2020-10-13 00:37:13 +0700

    login and logout

--- app/Http/Controllers/Auth/LogoutController.php
+++ app/Http/Controllers/Auth/LogoutController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 
 class LogoutController extends Controller
 {
+    public function index()
+    {
+        auth()->logout();
 
-    //
+        return redirect()->route('home');
+    }
 }
