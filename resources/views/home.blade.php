fa84b3e041459b20be30fa54407a1de60d8e8ab1
wannabe2266 <wannasiri.somb@dome.tu.ac.th>
2020-12-10 11:56:29 +0700

    edit menu

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,13 +5,13 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading"></div>
+                <div class="panel-heading">เมนู</div>
                 <div class="panel-body">
 
                     <!--button link to income page-->
                     <form class="form-horizontal" role="form" method="get" action="{{ route('income') }}">
                         <div class="form-group">
-                            <div class="col-md-4 col-md-offset-4"> <br>
+                            <div class="col-md-4 col-md-offset-4"><br>
                                 <button type="submit" class="btn btn-success btn-lg btn-block">บันทึกรายรับ</button>
                             </div>
                         </div>
