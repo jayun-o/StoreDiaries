c17231842c37d212d9e93f57e42eda069255a890
Thanchanok-yoo <thanchanok.yoo@dome.tu.ac.th>
2020-12-15 00:36:18 +0700

    แก้ไข panel-heading ที่ view_report.barchart

--- resources/views/report/barchart.blade.php
+++ resources/views/report/barchart.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-            <div class="panel-heading">กราฟแท่งรายงานรายรับ-รายจ่าย</div>
+            <div class="panel-heading">กราฟแท่งเปรียบเทียบรายรับ-รายจ่าย</div>
 
                 <div class="panel-body">
                 @if (session('status'))
